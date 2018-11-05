815c5c6993e869374565cbf0acbf6790dde2a726
Bianca Castro <biiianca_00@hotmail.com>
2018-11-05 18:46:23 +0100

    objects falling and collisions ok

--- game.js
+++ game.js
@@ -1,64 +1,74 @@
+function Game(canvas) {
+  this.canvas = document.getElementById(canvas);
+  this.ctx = this.canvas.getContext("2d");
+  this.fps = 60;
 
-function Game (canvas) {
-    this.canvas =document.getElementById(canvas);
-    this.ctx=this.canvas.getContext('2d'); 
-   this.fps=60;
-    
-    this.reset();
-    this.generateObjects();
-   
+  this.reset();
+  this.generateObjects();
 }
 
-Game.prototype.reset= function(){
-   this.background= new Background(this);
-    this.player = new Player(this);
-    this.objects=  [];
-    
-    
-}
-Game.prototype.generateObjects= function(){
-    this.objects.push(new Microsoft(this));
-    this.objects.push(new Apple(this));
-}
+Game.prototype.reset = function() {
+  this.background = new Background(this);
+  this.player = new Player(this);
+  this.objects = [];
+};
 
-Game.prototype.clearObjects=function(){
-    this.objects.filter(function(object){
-        return object.y>=0;
-    })
-}
+Game.prototype.generateObjects = function() {
+  this.objects.push(new Microsoft(this));
+  this.objects.push(new Apple(this));
+};
+
+Game.prototype.clearObjects = function() {
+  this.objects.filter(function(object) {
+    return object.y >= 0;
+  });
+};
+
 Game.prototype.clear = function() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }; 
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+};
 
+Game.prototype.draw = function() {
+  this.background.draw();
+  this.player.draw();
+  this.objects.forEach(function(object) {
+    object.draw();
+  });
+};
 
-Game.prototype.draw= function(){
-   this.background.draw();
-    this.player.draw();
-   this.objects.forEach(function(object){
-       object.draw();
-   })
-}
+Game.prototype.collision = function() {
+  return this.objects.some(
+    function(object) {
+      if (
+        this.player.x + this.player.w >= object.x &&
+        this.player.x < object.x + object.w &&
+        this.player.y + this.player.h >= object.y &&
+        object.y + object.h >= this.player.y
+      ){
+          this.objects.splice(this.objects.indexOf(object), 1)
+          return true
+      }
+    }.bind(this)
+  );
+};
 
- 
-Game.prototype.collision= function(){
+Game.prototype.move = function() {
+  this.player.move();
+  this.objects.forEach(function(object) {
+    object.move();
+  });
+};
 
-  }
-
-Game.prototype.move= function(){
-    this.player.move();
-    this.objects.forEach(function(object){
-        object.move();
-    })
-}
-
-Game.prototype.start=function(){
-this.interval=setInterval(function(){
-   this.clear();
-    this.draw();
-    this.move();
-
-   }.bind(this), 1000/this.fps);
-
-
-   
-}
+Game.prototype.start = function() {
+  this.interval = setInterval(
+    function() {
+      this.clear();
+      this.draw();
+      this.move();
+      if (this.collision()) {
+      
+      }
+    }.bind(this),
+    1000 / this.fps
+  );
+};
